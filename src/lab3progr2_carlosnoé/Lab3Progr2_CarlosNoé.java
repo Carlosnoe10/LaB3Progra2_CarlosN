b2cc70988955e387303e9381d1e8dcab95755e44
Carlosnoe10 <carlosdnoem@gmail.com>
2023-08-04 14:38:17 -0700

    Termine de hacer todos los agregar vamos avanzando creo que voy a seguir por eliminar y despues el Generar boleta so dejare lo de Mod para el final porque es lo mas tardado :P

--- src/lab3progr2_carlosnoé/Lab3Progr2_CarlosNoé.java
+++ src/lab3progr2_carlosnoé/Lab3Progr2_CarlosNoé.java
@@ -1,12 +1,19 @@
 package lab3progr2_carlosnoé;
 
+import java.awt.Color;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Scanner;
+import javax.swing.JColorChooser;
+import javax.swing.JOptionPane;
 
 public class Lab3Progr2_CarlosNoé {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         ArrayList<Vehiculos> ListaV = new ArrayList();
         Scanner k1ng = new Scanner(System.in);
         System.out.println("Bienvenid al menu \n"
@@ -25,13 +32,95 @@ public class Lab3Progr2_CarlosNoé {
                     x++;
                     switch (menu) {
                         case 1:
+                            
+                            System.out.println("Ingrese el numero de placa");
+                            String Placa = k1ng.nextLine();
+                            System.out.println("Ingrese la Marca");
+                            String Marca = k1ng.nextLine();
+                            System.out.println("Ingrese el tipo 'T'= Turism, 'C'=Camioneta, etc");
+                            char Type = k1ng.next().charAt(0);
+                            JColorChooser colorChooser = new JColorChooser();
+                            Color color = colorChooser.getColor();
+                            System.out.println("Ingrese el dia de nacimiento: ");
+                            String dia = k1ng.nextLine();
+                            System.out.println("Ingrese el mes de nacimiento: ");
+                            String mes = k1ng.nextLine();
+                            System.out.println("Ingrese el año de nacimiento");
+                            String ano = k1ng.nextLine();
+                            String entrada = dia + "/" + mes + "/" + ano;
+                            DateFormat format = new SimpleDateFormat("DD/MM/YYYY");
+                            Date fecha = format.parse(entrada);
 
+                            System.out.println("Ingrese el tipo de combustible del AutoMovil");
+                            String Cumbustible = k1ng.nextLine();
+                            System.out.println("Ingrese el numero de puertas: ");
+                            int NumPuertas = k1ng.nextInt();
+                            System.out.println("Ingrese el tipo de transmicion: ");
+                            String Trans = k1ng.nextLine();
+                            System.out.println("Ingrese el numero de puertas: ");
+                            int NumAsientos = k1ng.nextInt();
+
+                            Automovil Movil = new Automovil(Cumbustible, Trans, NumPuertas, NumAsientos, Placa, Marca, Type, color, fecha);
+                            ListaV.add(Movil);
                             break;
                         case 2:
 
+                            System.out.println("Ingrese el numero de placa");
+                            String Placa3 = k1ng.nextLine();
+                            System.out.println("Ingrese la Marca");
+                            String Marca3 = k1ng.nextLine();
+                            System.out.println("Ingrese el tipo 'T'= Turism, 'C'=Camioneta, etc");
+                            char Type3 = k1ng.next().charAt(0);
+                            JColorChooser colorChooser3 = new JColorChooser();
+                            Color color3 = colorChooser3.getColor();
+                            System.out.println("Ingrese el dia de nacimiento: ");
+                            String dia3 = k1ng.nextLine();
+                            System.out.println("Ingrese el mes de nacimiento: ");
+                            String mes3 = k1ng.nextLine();
+                            System.out.println("Ingrese el año de nacimiento");
+                            String ano3 = k1ng.nextLine();
+                            String entrada3 = dia3 + "/" + mes3 + "/" + ano3;
+                            DateFormat format3 = new SimpleDateFormat("DD/MM/YYYY");
+                            Date fecha3 = format3.parse(entrada3);
+
+                            System.out.println("Ingrese el limite");
+                            double lim = k1ng.nextDouble();
+                            System.out.println("Ingrese el Peso");
+                            double peso = k1ng.nextDouble();
+                            System.out.println("Ingrese el Consumo");
+                            double consumo = k1ng.nextDouble();
+
+                            Moto mum = new Moto(lim, peso, consumo, Placa3, Marca3, Type3, color3, fecha3);
+                            ListaV.add(mum);
                             break;
                         case 3:
+                            System.out.println("Ingrese el numero de placa");
+                            String Placa1 = k1ng.nextLine();
+                            System.out.println("Ingrese la Marca");
+                            String Marca1 = k1ng.nextLine();
+                            System.out.println("Ingrese el tipo 'T'= Turism, 'C'=Camioneta, etc");
+                            char Type1 = k1ng.next().charAt(0);
+                            JColorChooser colorChooser1 = new JColorChooser();
+                            Color color1 = colorChooser1.getColor();
+                            System.out.println("Ingrese el dia de nacimiento: ");
+                            String dia1 = k1ng.nextLine();
+                            System.out.println("Ingrese el mes de nacimiento: ");
+                            String mes1 = k1ng.nextLine();
+                            System.out.println("Ingrese el año de nacimiento");
+                            String ano1 = k1ng.nextLine();
+                            String entrada1 = dia1 + "/" + mes1 + "/" + ano1;
+                            DateFormat format1 = new SimpleDateFormat("DD/MM/YYYY");
+                            Date fecha1 = format1.parse(entrada1);
 
+                            System.out.println("Ingrese la capacidad de pasajeros");
+                            int pass = k1ng.nextInt();
+                            System.out.println("Ingrese el numeroDEejes");
+                            int NumEJE = k1ng.nextInt();
+                            System.out.println("Ingrese el Consumo");
+                            double consumo1 = k1ng.nextDouble();
+
+                            Buses bus = new Buses(pass, NumEJE, consumo1, Placa1, Marca1, Type1, color1, fecha1);
+                            ListaV.add(bus);
                             break;
                         case 4:
                             break;
@@ -67,12 +156,60 @@ public class Lab3Progr2_CarlosNoé {
 
         System.out.println("Fin");
     }
-    public static void ListarVehi(ArrayList<Vehiculos> ListaV){
-        for (Iterator<Vehiculos> iterator = ListaV.iterator(); iterator.hasNext();) {
-            
-            Vehiculos next = iterator.next();
-            System.out.println(next);
+
+    public static ArrayList<Vehiculos> AgregarAutomovil(ArrayList<Vehiculos> ListaV) throws ParseException {
+        Scanner k1ng = new Scanner(System.in);
+        System.out.println("Ingrese el numero de placa");
+        String Placa = k1ng.nextLine();
+        System.out.println("Ingrese la Marca");
+        String Marca = k1ng.nextLine();
+        System.out.println("Ingrese el tipo 'T'= Turism, 'C'=Camioneta, etc");
+        char Type = k1ng.next().charAt(0);
+        JColorChooser colorChooser = new JColorChooser();
+        Color color = colorChooser.getColor();
+        System.out.println("Ingrese el dia de nacimiento: ");
+        String dia = k1ng.nextLine();
+        System.out.println("Ingrese el mes de nacimiento: ");
+        String mes = k1ng.nextLine();
+        System.out.println("Ingrese el año de nacimiento");
+        String ano = k1ng.nextLine();
+        String entrada = dia + "/" + mes + "/" + ano;
+        DateFormat format = new SimpleDateFormat("DD/MM/YYYY");
+        Date fecha = format.parse(entrada);
+
+        System.out.println("Ingrese el tipo de combustible del AutoMovil");
+        String Cumbustible = k1ng.nextLine();
+        System.out.println("Ingrese el numero de puertas: ");
+        int NumPuertas = k1ng.nextInt();
+        System.out.println("Ingrese el tipo de transmicion: ");
+        String Trans = k1ng.nextLine();
+        System.out.println("Ingrese el numero de puertas: ");
+        int NumAsientos = k1ng.nextInt();
+
+        Automovil Movil = new Automovil(Cumbustible, Trans, NumPuertas, NumAsientos, Placa, Marca, Type, color, fecha);
+        ListaV.add(Movil);
+        return ListaV;
+
+    }
+
+    public static void ListarVehi(ArrayList<Vehiculos> ListaV) {
+        System.out.println("Vehiculos");
+        for (Vehiculos vehiculos : ListaV) {
+            if (vehiculos instanceof Automovil) {
+                System.out.println(vehiculos.toString());
+            }
         }
-        
+        System.out.println("Motocicletas");
+        for (Vehiculos vehiculos : ListaV) {
+            if (vehiculos instanceof Moto) {
+                System.out.println(vehiculos.toString());
+            }
+        }
+        System.out.println("Buses");
+        for (Vehiculos vehiculos : ListaV) {
+            if (vehiculos instanceof Buses) {
+                System.out.println(vehiculos.toString());
+            }
+        }
     }
 }
