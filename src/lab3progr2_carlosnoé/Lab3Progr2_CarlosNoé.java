a02d253e59e72159f5d6457e88348769fc668382
Carlosnoe10 <carlosdnoem@gmail.com>
2023-08-04 16:21:29 -0700

    Termine (〃￣︶￣)人(￣︶￣〃)

--- src/lab3progr2_carlosnoé/Lab3Progr2_CarlosNoé.java
+++ src/lab3progr2_carlosnoé/Lab3Progr2_CarlosNoé.java
@@ -62,7 +62,7 @@ public class Lab3Progr2_CarlosNoé {
                             System.out.println("Ingrese el numero de puertas: ");
                             int NumAsientos = k1ng.nextInt();
 
-                            Automovil Movil = new Automovil(Cumbustible, Trans, NumPuertas, NumAsientos, Placa, Marca,Modelo, Type, color, fecha);
+                            Automovil Movil = new Automovil(Cumbustible, Trans, NumPuertas, NumAsientos, Placa, Marca, Modelo, Type, color, fecha);
                             ListaV.add(Movil);
                             break;
                         case 2:
@@ -94,7 +94,7 @@ public class Lab3Progr2_CarlosNoé {
                             System.out.println("Ingrese el Consumo");
                             double consumo = k1ng.nextDouble();
 
-                            Moto mum = new Moto(lim, peso, consumo, Placa3, Modelo1,Marca3, Type3, color3, fecha3);
+                            Moto mum = new Moto(lim, peso, consumo, Placa3, Modelo1, Marca3, Type3, color3, fecha3);
                             ListaV.add(mum);
                             break;
                         case 3:
@@ -125,7 +125,7 @@ public class Lab3Progr2_CarlosNoé {
                             System.out.println("Ingrese el Consumo");
                             double consumo1 = k1ng.nextDouble();
 
-                            Buses bus = new Buses(pass, NumEJE, consumo1, Placa1,Modelo2, Marca1, Type1, color1, fecha1);
+                            Buses bus = new Buses(pass, NumEJE, consumo1, Placa1, Modelo2, Marca1, Type1, color1, fecha1);
                             ListaV.add(bus);
                             break;
                         case 4:
@@ -134,13 +134,13 @@ public class Lab3Progr2_CarlosNoé {
                             }
                             System.out.println("Ingrese el numero del carro que desea Pagar");
                             int numero3 = k1ng.nextInt();
-
+                            MenuCambio(ListaV, numero3);
                             if (ListaV.get(numero3) instanceof Automovil) {
-                                Xpagar = 275 + 250 + 1200;
+                                MenuCambioAutomovil(ListaV, numero3);
                             } else if (ListaV.get(numero3) instanceof Moto) {
-                                Xpagar = 275 + 250 + 200;
+                                MenuCambioMoto(ListaV, numero3);
                             } else {
-                                Xpagar = 275 + 250 + 1000;
+                                MenuCambioBuses(ListaV, numero3);
                             }
 
                             break;
@@ -198,9 +198,7 @@ public class Lab3Progr2_CarlosNoé {
         System.out.println("Fin");
     }
 
-
-
-    public static void MenuCambio(ArrayList<Vehiculos> ListaV, int num) {
+    public static void MenuCambio(ArrayList<Vehiculos> ListaV, int num) throws ParseException {
         Scanner k1ng = new Scanner(System.in);
         System.out.println("Bienvenid al menu \n"
                 + "1. MOD Placa \n"
@@ -234,21 +232,182 @@ public class Lab3Progr2_CarlosNoé {
                         case 4:
                             System.out.println("Ingrese el nuevo tipo");
                             char Type1 = k1ng.next().charAt(0);
-                            ListaV.get(num).getTipo(Type1);
+                            ListaV.get(num).setTipo(Type1);
                             break;
                         case 7:
-
+                            JColorChooser colorChooser1 = new JColorChooser();
+                            Color color1 = colorChooser1.getColor();
+                            ListaV.get(num).setColor(color1);
                             break;
                         case 6:
-
+                            System.out.println("Ingrese el dia de nacimiento: ");
+                            String dia1 = k1ng.nextLine();
+                            System.out.println("Ingrese el mes de nacimiento: ");
+                            String mes1 = k1ng.nextLine();
+                            System.out.println("Ingrese el año de nacimiento");
+                            String ano1 = k1ng.nextLine();
+                            String entrada1 = dia1 + "/" + mes1 + "/" + ano1;
+                            DateFormat format1 = new SimpleDateFormat("DD/MM/YYYY");
+                            Date fecha1 = format1.parse(entrada1);
+                            ListaV.get(num).setAnio(fecha1);
                             break;
                     }// switch menu p
                     System.out.println("Bienvenid al menu \n"
-                            + "1. CRUD Concecionaria \n"
-                            + "2. CRUD Clientes \n"
-                            + "3. CRUD Vehiculos \n"
-                            + "4. Compra y venta de vehiculos\n"
-                            + "5. Salida");
+                            + "1. MOD Placa \n"
+                            + "2. MOD Marca \n"
+                            + "3. MOD Modelo \n"
+                            + "4. MOD Tipo\n"
+                            + "5. MOD Color\n"
+                            + "6. MOD Anio \n"
+                            + "7. Otro");
+                    menu = k1ng.nextInt();
+
+                } else {
+                    System.out.println("Ingrese adecuadamente el numero");
+                    x--;
+                }//if
+            }//while
+        }//for
+
+        System.out.println("Fin");
+    }
+
+    public static void MenuCambioBuses(ArrayList<Vehiculos> ListaV, int num) {
+        Scanner k1ng = new Scanner(System.in);
+        System.out.println("Bienvenid al menu \n"
+                + "1. Cambiar la capacidad de pasajeros \n"
+                + "2. Cambiar el numero de ejes\n"
+                + "3. Cambiar la longitud \n"
+                + "4. Salida");
+        int menu = k1ng.nextInt();
+        for (int x = 0; x < 1; x++) {
+            while (menu != 4) {
+                if ((menu > 0) && (menu < 5)) { // 4 es igual al numero limite que deseo agregar
+                    x++;
+                    switch (menu) {
+                        case 1:
+                            System.out.println("Ingrese el Nuevo valor de la capacidad de pasajeros: ");
+                            int newLim = k1ng.nextInt();
+                            ((Buses) ListaV.get(num)).setCapacidadPassa(newLim);
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el Nuevo valor del numero de ejes");
+                            int newPeso = k1ng.nextInt();
+                            ((Buses) ListaV.get(num)).setNumeroEjes(newPeso);
+                            break;
+                        case 3:
+                            System.out.println("Ingrese el Nuevo valor de la longitud");
+                            double newConsu1 = k1ng.nextDouble();
+                            ((Buses) ListaV.get(num)).setLongitud(newConsu1);
+                            break;
+
+                    }// switch menu p
+                    System.out.println("Bienvenid al menu \n"
+                            + "1. Cambiar la capacidad de pasajeros \n"
+                            + "2. Cambiar el numero de ejes\n"
+                            + "3. Cambiar la longitud \n"
+                            + "4. Salida");
+                    menu = k1ng.nextInt();
+
+                } else {
+                    System.out.println("Ingrese adecuadamente el numero");
+                    x--;
+                }//if
+            }//while
+        }//for
+
+        System.out.println("Fin");
+    }
+
+    public static void MenuCambioMoto(ArrayList<Vehiculos> ListaV, int num) {
+        Scanner k1ng = new Scanner(System.in);
+        System.out.println("Bienvenid al menu \n"
+                + "1. Cambiar Limite de velocidad \n"
+                + "2. Cambiar el peso \n"
+                + "3. Cambiar el consumo de combustible \n"
+                + "4. Salida");
+        int menu = k1ng.nextInt();
+        for (int x = 0; x < 1; x++) {
+            while (menu != 4) {
+                if ((menu > 0) && (menu < 5)) { // 4 es igual al numero limite que deseo agregar
+                    x++;
+                    switch (menu) {
+                        case 1:
+                            System.out.println("Ingrese el Nuevo valor del limite de velocidad");
+                            double newLim = k1ng.nextDouble();
+                            ((Moto) ListaV.get(num)).setLimVeloz(newLim);
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el Nuevo valor del Peso");
+                            double newPeso = k1ng.nextDouble();
+                            ((Moto) ListaV.get(num)).setPeso(newPeso);
+                            break;
+                        case 3:
+                            System.out.println("Ingrese el Nuevo valor del consumo de combustible");
+                            double newConsu = k1ng.nextDouble();
+                            ((Moto) ListaV.get(num)).setConsumoDCumbus(newConsu);
+                            break;
+
+                    }// switch menu p
+                    System.out.println("Bienvenid al menu \n"
+                            + "1. Cambiar Limite de velocidad \n"
+                            + "2. Cambiar el peso \n"
+                            + "3. Cambiar el consumo de combustible \n"
+                            + "4. Salida");
+                    menu = k1ng.nextInt();
+
+                } else {
+                    System.out.println("Ingrese adecuadamente el numero");
+                    x--;
+                }//if
+            }//while
+        }//for
+
+        System.out.println("Fin");
+    }
+
+    public static void MenuCambioAutomovil(ArrayList<Vehiculos> ListaV, int num) {
+        Scanner k1ng = new Scanner(System.in);
+        System.out.println("Bienvenid al menu \n"
+                + "1. Cambiar Tipo Combustible \n"
+                + "2. Cambiar Tipo de transporte \n"
+                + "3. Cambiar Numero de puertas \n"
+                + "4. Cambiar Numero de asientos \n"
+                + "5. Otro");
+        int menu = k1ng.nextInt();
+        for (int x = 0; x < 1; x++) {
+            while (menu != 5) {
+                if ((menu > 0) && (menu < 6)) { // 4 es igual al numero limite que deseo agregar
+                    x++;
+                    switch (menu) {
+                        case 1:
+                            System.out.println("Ingrese el nuevo tipo de combustible: ");
+                            String Cum = k1ng.nextLine();
+                            ((Automovil) ListaV.get(num)).setTipoCombu(Cum);
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el nuevo tipo de Transporte: ");
+                            String Trans = k1ng.nextLine();
+                            ((Automovil) ListaV.get(num)).setTipoTrans(Trans);
+                            break;
+                        case 3:
+                            System.out.println("Ingrese el numero de puertas");
+                            int Puertas = k1ng.nextInt();
+                            ((Automovil) ListaV.get(num)).setNumeroDePuertas(Puertas);
+                            break;
+                        case 4:
+                            System.out.println("Ingrese el numero de Asientos");
+                            int Asientos = k1ng.nextInt();
+                            ((Automovil) ListaV.get(num)).setNumeroDeAsientos(Asientos);
+                            break;
+
+                    }// switch menu p
+                    System.out.println("Bienvenid al menu \n"
+                            + "1. Cambiar Tipo Combustible \n"
+                            + "2. Cambiar Tipo de transporte \n"
+                            + "3. Cambiar Numero de puertas \n"
+                            + "4. Cambiar Numero de asientos \n"
+                            + "5. Otro");
                     menu = k1ng.nextInt();
 
                 } else {
